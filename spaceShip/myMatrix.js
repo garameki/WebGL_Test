29f2d5f47ec8926447104021f1250ab06a6c5f52
garameki <garameki@users.noreply.github.com>
2018-04-14 09:36:46 +0900

    add loadZero()

--- spaceShip/myMatrix.js
+++ spaceShip/myMatrix.js
@@ -15,6 +15,7 @@
 	//mymat4
 	myMat4 = { };
 	Object.defineProperty(myMat4,'loadIdentity'	,{value:prepareAM,writable:false,enumerable:true,configurable:false});
+	Object.defineProperty(myMat4,'loadZero'		,{value:clearToZeroAM,writable:false,enumerable:true,configurable:false});
 	Object.defineProperty(myMat4,'loadPerspective'	,{value:preparePM,writable:false,enumerable:true,configurable:false});
 	Object.defineProperty(myMat4,'trans'		,{value:translateAM,writable:false,enumerable:true,configurable:false});
 	Object.defineProperty(myMat4,'rot'		,{value:rotateAM,writable:false,enumerable:true,configurable:false});
@@ -62,6 +63,18 @@
 	var _x11=void 0,_x12=void 0,_x13=void 0,_x14=void 0,_x21=void 0,_x22=void 0,_x23=void 0,_x24=void 0,_x31=void 0,_x32=void 0,_x33=void 0,_x34=void 0,_x41=void 0,_x42=void 0,_x43=void 0,_x44=void 0;//target matrix
 	var _s11=void 0,_s12=void 0,_s13=void 0,_s14=void 0,_s21=void 0,_s22=void 0,_s23=void 0,_s24=void 0,_s31=void 0,_s32=void 0,_s33=void 0,_s34=void 0,_s41=void 0,_s42=void 0,_s43=void 0,_s44=void 0;//stack matrix
 	var _a11=void 0,_a12=void 0,_a13=void 0,_a14=void 0,_a21=void 0,_a22=void 0,_a23=void 0,_a24=void 0,_a31=void 0,_a32=void 0,_a33=void 0,_a34=void 0,_a41=void 0,_a42=void 0,_a43=void 0,_a44=void 0;//answer matrix
+	/**
+	 *
+	 *make accumeration matrix Zero matrix
+	 *
+	*/
+	function clearToZeroAM(){//createAccumerateMatrix as Unit Matrix
+
+		_a11 = 0;	_a12 = 0;	_a13 = 0;	_a14 = 0;
+		_a21 = 0;	_a22 = 0;	_a23 = 0;	_a24 = 0;
+		_a31 = 0;	_a32 = 0;	_a33 = 0;	_a34 = 0;
+		_a41 = 0;	_a42 = 0;	_a43 = 0;	_a44 = 0;
+	};
 	/**
 	 *
 	 *Prepare accumerate matrix
