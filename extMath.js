da8fdf8f65798466d86bad2c645ac0a81bab9756
garameki <garameki@users.noreply.github.com>
2018-06-12 22:10:19 +0900

    Add a function   to get 8bit or 24bit normalized decimal number from binary number 	new file:   extMath.js

---
+++ extMath.js
@@ -0,0 +1,11 @@
+extMath=null;// This is dummy. Because there is not 'extMath' object.
+libFileRelationship.create('extMath');
+
+Math.normalize8 = function(num){
+	return num/0xFF;
+};
+Math.normalize24 = function(num){
+	return num/0xFFFFFF;
+};
+
+
