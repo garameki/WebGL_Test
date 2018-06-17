e29faa1ba186e36e561f2046052ec03c65d256c2
garameki <garameki@users.noreply.github.com>
2018-06-17 18:55:38 +0900

    Modify key operation   to implement uniform linear motion   Key operation     Shft + arrow ... rotate view towarded to Up,Down,Right or Left     Q,W ............ rotate view towarded to Font or Back     arrow .......... injection towards to Up,Down,Right or Left     A,Z ............ injection towards to Front or Back
     Issue
       the Saturn is not drawn in screen.
    
     File
    	modified:   myXYZManipulation.js
    	modified:   spaceShipZZ.htm

--- myXYZManipulation.js
+++ myXYZManipulation.js
@@ -11,32 +11,42 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 		/** global scope **/
 		myXYZManipulation = myXYZMani = { };
 
+		//differential
+		const dTurn = 0.01;//angle
+		const dInject = 0.01;//Accelaration
+
 		//key codes
-		const R=39;//→
-		const L=37;//←
-		const U=38;//↑
-		const D=40;//↓
+		const AR=39;//→ Arrow Right
+		const AL=37;//←
+		const AU=38;//↑
+		const AD=40;//↓
 //		const SP=32;//space key
 		const A=65;//a
 		const Z=90;//z
 		const W=87;//w
 		const Q=81;//q
+		const SH=16;//shift key
 	
-		//動き(eventListener)
-		var gDSpeed   = 0;
-		var gDRoll    = 0;
-		var gDTurnUD  = 0;
-		var gDTurnLR  = 0;
+		//Inject (eventListener)
+		var gInjectFB  = 0;
+		var gInjectUD  = 0;
+		var gInjectLR  = 0;
+
+		//Rolling (eventListener)
+		var gTurnAxZ = 0;//roll with axis Z
+		var gTurnAxX = 0;//roll with axis X
+		var gTurnAxY = 0;//roll with axis Y
 
 //		var gMissile=false;
-		var gR=false;
-		var gL=false;
-		var gU=false;
-		var gD=false;
+		var gAR=false;
+		var gAL=false;
+		var gAU=false;
+		var gAD=false;
 		var gA=false;
 		var gZ=false;
 		var gW=false;
 		var gQ=false;
+		var gSH=false;
 
 		var Member = function(){
 			myXYZ.SuperMember.call(this);
@@ -48,17 +58,10 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 			this.speed = [0,0,0];
 
 			//axiz to rotate
-			this.frontX = 0;
-			this.frontY = 0;
-			this.frontZ = 1;
-			this.topX=0;
-			this.topY=1;
-			this.topZ=0;
-			this.rightX=1;
-			this.rightY=0;
-			this.rightZ=0;
+			this.frontAxis = [0, 0,-1];
+			this.topAxis   = [0, 1, 0];
+			this.rightAxis = [1, 0, 0];
 
-			this.ratioR=0.01;
 
 			this.matAccume=[1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1];//Identity
 			this.matAccumeNotTranslated=[1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1];//Identity
@@ -66,11 +69,44 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 		};
 		myXYZ.inherits(Member);
 		Member.prototype.upToDate = function(){
-			this.speed[2] += gDSpeed * 0.01;
-			this.thetaLR  = -gDTurnLR*this.ratioR;
-			this.thetaUD  = -gDTurnUD*this.ratioR;
-			this.thetaRoll=  gDRoll  *this.ratioR;
+			this.speed.x = this.speed.x + gInjectLR;
+			this.speed.y = this.speed.y + gInjectUD;
+			this.speed.z = this.speed.z + gInjectFB;
+
+	//		this.speed[2] += gDSpeed * 0.01;
+			this.thetaLR  = gTurnAxY;
+			this.thetaUD  = gTurnAxX;
+			this.thetaRoll= gTurnAxZ;
+	///		this.turnLR();
+	///		this.turnUD();
+	///		this.roll();
 		};
+		Member.prototype.turnLR = function(){
+			this.frontAxis = myMat4.rotXYZ(0,1,0,this.thetaLR,this.frontAxis.x,this.frontAxis.y,this.frontAxis.z)
+			var ux=this.frontAxis.x,uy=this.frontAxis.y,uz=this.frontAxis.z,vx=this.topAxis.x,vy=this.topAxis.y,vz=this.topAxis.z;
+			this.rightAxis.x=uy*vz-uz*vy;
+			this.rightAxis.y=uz*vx-ux*vz;
+			this.rightAxis.z=ux*vy-uy*vx;
+		};
+		Member.prototype.turnUD = function(){
+			this.forntAxis = myMat4.rotXYZ(1,0,0,this.thetaUD,this.frontAxis.x,this.frontAxis.y,this.frontAxis.z)
+			var ux=this.rightAxis.x,uy=this.rightAxis.y,uz=this.rightAxis.z,vx=this.frontAxis.x,vy=this.frontAxis.y,vz=this.frontAxis.z;
+			this.topAxis.x=uy*vz-uz*vy;
+			this.topAxis.y=uz*vx-ux*vz;
+			this.topAxis.z=ux*vy-uy*vx;
+		};
+		Member.prototype.roll = function(){
+
+			var topAxis = myMat4.rotXYZ(0,0,1,this.thetaRoll,0,this.topAxis.y,this.topAxis.z)
+			var ux=this.frontAxis.x,uy=this.frontAxis.y,uz=this.frontAxis.z,vx=this.topAxis.x,vy=this.topAxis.y,vz=this.topAxis.z;
+			this.rightAxis.x=uy*vz-uz*vy;
+			this.rightAxis.y=uz*vx-ux*vz;
+			this.rightAxis.z=ux*vy-uy*vx;
+
+		};
+
+
+
 
 		var hero = new Member();//only one member is allowed to be made
 		var flagSeatReserved = false;
@@ -90,63 +126,64 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 
 
 		Object.defineProperty(myXYZMani,'move',{value:move(hero)});
-		function move(member){
+		function move(member){//member === hero
 			var sumRemainder=0;
 			return function(dt){
 				sumRemainder+=dt;
 				var n = Math.floor(sumRemainder/drawStep);//何回移動させるか
 				sumRemainder=sumRemainder%drawStep;
 
-
 				//accumerate all motions
 				for(var ii=0;ii<n;ii++){
-
 
 					member.upToDate();
 	
 					myMat4.load(member.matAccume);
 						myMat4.trans(-member.speed.x,-member.speed.y,-member.speed.z);//これがなければいつも前に表示される
-						myMat4.rot(member.rightX,member.rightY,member.rightZ,member.thetaUD);
-						myMat4.rot(member.topX,member.topY,member.topZ,member.thetaLR);
-						myMat4.rot(member.frontX,member.frontY,member.frontZ,member.thetaRoll);
+						myMat4.rot(0,1,0,member.thetaLR);
+						myMat4.rot(1,0,0,member.thetaUD);
+						myMat4.rot(0,0,1,member.thetaRoll);
 					myMat4.storeTo(member.matAccume);
 
 					//accumerate rotation only
 					myMat4.load(member.matAccumeNotTranslated);
-						//myMat4.trans(-member.speedX,-member.speedY,-member.speedZ);//これがなければいつも前に表示される
-						myMat4.rot(member.rightX,member.rightY,member.rightZ,member.thetaUD);
-						myMat4.rot(member.topX,member.topY,member.topZ,member.thetaLR);
-						myMat4.rot(member.frontX,member.frontY,member.frontZ,member.thetaRoll);
+						//myMat4.trans(-member.speed.x,-member.speed.y,-member.speed.z);//これがなければいつも前に表示される
+						myMat4.rot(0,1,0,member.thetaLR);
+						myMat4.rot(1,0,0,member.thetaUD);
+						myMat4.rot(0,0,1,member.thetaRoll);
 					myMat4.storeTo(member.matAccumeNotTranslated);
+
+
 	
 					//accumerate translation only
 					myMat4.load(member.matAccumeNotRotated);
-						myMat4.trans(-member.speedX,-member.speedY,-member.speedZ);//これがなければいつも前に表示される
-						//myMat4.rot(member.rightX,member.rightY,member.rightZ,member.thetaUD);
-						//myMat4.rot(member.topX,member.topY,member.topZ,member.thetaLR);
-						//myMat4.rot(member.frontX,member.frontY,member.frontZ,member.thetaRoll);
+						myMat4.trans(-member.speed.x,-member.speed.y,-member.speed.z);//これがなければいつも前に表示される
+						//myMat4.rot(0,1,0,member.thetaLR);
+						//myMat4.rot(1,0,0,member.thetaUD);
+						//myMat4.rot(0,0,1,member.thetaRoll);
 					myMat4.storeTo(member.matAccumeNotRotated);
 
 					myMat4.loadIdentity();
 						//don't touch here
-							myMat4.rot(member.rightX,member.rightY,member.rightZ,member.thetaUD);
-							myMat4.rot(member.topX,member.topY,member.topZ,member.thetaLR);
-							myMat4.rot(member.frontX,member.frontY,member.frontZ,member.thetaRoll);
+							myMat4.rot(0,1,0,-member.thetaLR);
+							myMat4.rot(1,0,0,-member.thetaUD);
+							myMat4.rot(0,0,1,member.thetaRoll);
 						//
 
-						const len = member.speed.length3D;
+						//const len = member.speed.length3D;
 						const x = member.speed.x;const y = member.speed.y;const z = member.speed.z;
-						const a=myMat4.arr;
+						const a = myMat4.arr;
 						member.speed.x = a[0]*x+a[1]*y+a[2]*z+a[3];
 						member.speed.y = a[4]*x+a[5]*y+a[6]*z+a[7];
 						member.speed.z = a[8]*x+a[9]*y+a[10]*z+a[11];
-						member.speed.normalize3D();
-						member.speed.mag3D(len);
+						//member.speed.normalize3D();//length3Dでspeed.x=speed.y=speed.z=0となるのでNaNになる
+						//member.speed.mag3D(len);
 
 				}
 			};
 		};
 
+		//Burner Could be only used
 		Object.defineProperty(myXYZMani,'accordingToKeyFlame',{value:accordingToKeyFlame});
 		function accordingToKeyFlame(){
 
@@ -156,24 +193,24 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 				if(false){
 					//nothing
 				
-				}else if(gR == 1){
+				}else if(gInjectLR < 0){
 					myXYZ.rotate(0,0,1,0,-90)(time);
 					myXYZ.translate(0.1,0,0)(time);
-				}else if(gL == 1){
+				}else if(gInjectLR > 0){
 					myXYZ.rotate(0,0,1,0,90)(time);
 					myXYZ.translate(-0.1,0,0)(time);
-				}else if(gU == 1){
+				}else if(gInjectUD < 0){
 					myXYZ.rotate(1,0,0,0,0)(time);
 					myXYZ.translate(0,0.1,0)(time);
-				}else if(gD == 1){
+				}else if(gInjectUD > 0){
 					myXYZ.rotate(1,0,0,0,180)(time);
 					myXYZ.translate(0,-0.1,0)(time);
-				}else if(gA == 1){
+				}else if(gInjectFB > 0){
 					myXYZ.rotate(1,0,0,0,-90)(time);
-				}else if(gZ == 1){
+				}else if(gInjectFB < 0){
 					myXYZ.rotate(1,0,0,0,90)(time);
 				}else{
-					 myXYZ.translate(0,0,1000)(time);
+					 myXYZ.translate(0,0,10)(time);
 				}
 			// to here
 			};
@@ -184,31 +221,37 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 		function keydown(event){
 			var key = event.keyCode;
 
-			if(key==R){
-				gDTurnLR=1;
-				gR=true;
-			}else if(key==L){
-				gDTurnLR=-1;
-				gL=true;
-			}else if(key==U){
-				gDTurnUD=-1;
-				gU=true;
-			}else if(key==D){
-				gDTurnUD=1;
-				gD=true;
+			if(key==SH){
+				gSH=true;
+			}else if(key==AR){
+				if(gSH) gTurnAxY=-dTurn;
+				else gInjectLR=-dInject;
+				gAR=true;
+			}else if(key==AL){
+				if(gSH) gTurnAxY=dTurn;
+				else gInjectLR=dInject;
+				gAL=true;
+			}else if(key==AU){
+				if(gSH) gTurnAxX = dTurn;
+				else gInjectUD=-dInject;
+				gAU=true;
+			}else if(key==AD){
+				if(gSH) gTurnAxX = -dTurn;
+				else gInjectUD=dInject;
+				gAD=true;
 //			}else if(key==SP){	
 //				if(gMissile==false)gMissile=true;
 			}else if(key==A){
-				gDSpeed=1;
+				gInjectFB=dInject;
 				gA=true;
 			}else if(key==Z){
-				gDSpeed=-1;
+				gInjectFB=-dInject;
 				gZ=true;
 			}else if(key==Q){
-				gDRoll=-1;
+				gTurnAxZ=-dTurn;
 				gQ=true;
 			}else if(key==W){
-				gDRoll=1;
+				gTurnAxZ=dTurn;
 				gW=true;
 			};
 		};
@@ -216,49 +259,65 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 		function keyup(event){
 			var key = event.keyCode;
 	
-			if(key==R){
-				if(gR){
-					gR=false;
+			if(key==SH){
+				gSH=false;
+				gInjectLR=0;
+				gInjectUD=0;
+				gInjectFB=0;
+				gTurnAxX=0;
+				gTurnAxY=0;
+				gTurnAxZ=0;
+			}else if(key==AR){
+				if(gAR){
+					gAR=false;
 				}else{
-					console.error("RのkeyDownを捉えていなかった");
+					console.error("ARのkeyDownを捉えていなかった");
 				};
-				if(gL){
-					gDTurnLR=1;
+				if(gAL){
+					if(gSH) gTurnAxY = -dTurn;
+					else gInjectLR = -dInject;
 				}else{
-					gDTurnLR=0;
+					gTurnAxY=0;
+					gInjectLR=0;
 				};
-			}else if(key==L){
-				if(gL){
-					gL=false;
+			}else if(key==AL){
+				if(gAL){
+					gAL=false;
 				}else{
-					console.error("LのkeyDownを捉えていなかった");
+					console.error("ALのkeyDownを捉えていなかった");
 				};
-				if(gR){
-					gDTurnLR=-1;
+				if(gAR){
+					if(gSH) gTurnAxY = dTurn;
+					else gInjectLR = dInject;
 				}else{
-					gDTurnLR=0;
+					gTurnAxY=0;
+					gInjectLR=0;
 				};
-			}else if(key==U){
-				if(gU){
-					gU=false;
+			}else if(key==AU){
+				if(gAU){
+					gAU=false;
 				}else{
-					console.error("UのkeyDownを捉えていなかった");
+					console.error("AUのkeyDownを捉えていなかった");
 				};
-				if(gD){
-					gDTurnUD=-1;
+				if(gAD){
+					if(gSH) gTurnAxX = -dTurn;
+					gInjectUD=-dInject;
 				}else{
-					gDTurnUD=0;
+					gTurnAxX=0;
+					gInjectUD=0;
 				};
-			}else if(key==D){
-				if(gD){
-					gD=false;
+			}else if(key==AD){
+				if(gAD){
+					gAD=false;
 				}else{
-					console.error("DのkeyDownを捉えていなかった");
+					console.error("ADのkeyDownを捉えていなかった");
 				};
-				if(gU){
-					gDTurnUD=1;
+				if(gAU){
+					if(gSH) gTurnAxX=dTurn;
+					gInjectUD=dInject;
 				}else{
-					gDTurnUD=0;
+					gTurnAxX=0;
+					gInjectUD=0;
 				};
 			}else if(key==A){
 				if(gA){
@@ -267,9 +326,9 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 					console.error("AのkeyDownを捉えていなかった");
 				};
 				if(gZ){
-					gDSpeed=-1;
+					gInjectFB=-dInject;
 				}else{
-					gDSpeed=0;
+					gInjectFB=0;
 				};
 			}else if(key==Z){
 				if(gZ){
@@ -278,9 +337,9 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 					console.error("ZのkeyDownを捉えていなかった");
 				};
 				if(gA){
-					gDSpeed=1;
+					gInjectFB=dInject;
 				}else{
-					gDSpeed=0;
+					gInjectFB=0;
 				};
 			}else if(key==W){
 				if(gW){
@@ -289,9 +348,9 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 					console.log("WのkeyDownを捉えていなかった");
 				}
 				if(gQ){
-					gDRoll=-1;
+					gTurnAxZ=-dTurn;
 				}else{
-					gDRoll=0;
+					gTurnAxZ=0;
 				}
 			}else if(key==Q){
 				if(gQ){
@@ -300,9 +359,9 @@ libFileRelationship.myXYZManipulation.relatedTo='myXYZ';
 					console.log("QのkeyDownを捉えていなかった");
 				}
 				if(gW){
-					gDRoll=1
+					gTurnAxZ=dTurn;
 				}else{
-					gDRoll=0;
+					gTurnAxZ=0;
 				}
 			};
 		};
